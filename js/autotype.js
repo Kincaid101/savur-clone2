ebaf4e021fa9a6dd86209aee7a91414e14162b95
kincaid101 <charlesgithinji41@gmail.com>
2023-09-12 11:51:51 +0300

    first commit

---
+++ js/autotype.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ["Thousands of offers.",'Discounts.', 'Quality products.',"Unique experiences."],
+    typeSpeed: 200,
+    backSpeed:200,
+    loop: true,
+    loopSpeed: 200,
+});
---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.getElementById("signup").onclick = function () {
+    window.location.href = "register.html";
+}
